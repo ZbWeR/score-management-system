83ab2954ff10694be90cd529cd348672c28323d5
だれ <32427878+imiwakanai@users.noreply.github.com>
2023-11-04 23:04:08 +0800

    🔧 build: 添加依赖 Tailwind CSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>Hello World!</main>
+  <h1 class="m-8 mb-5 text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div class="p-2 px-6 ml-6 font-bold text-white bg-blue-500 rounded-lg w-fit">
+    添加了 Tailwind CSS
+  </div>
 </template>

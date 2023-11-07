1294b41d919d31500bf71606b8deedf1ea28b090
だれ <32427878+imiwakanai@users.noreply.github.com>
2023-11-07 21:51:36 +0800

    ✨ feat: 添加 用户状态管理

---
+++ src/stores/user.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia'
+
+enum Roles {
+  Teacher = 'teacher',
+  Student = 'student'
+}
+
+interface IUserStore {
+  islogin: boolean
+  role: Roles | null
+  userid: string | null
+  username: string | null
+}
+
+export const useUserStore = defineStore({
+  id: 'user',
+  state: (): IUserStore => ({
+    islogin: false,
+    role: null,
+    userid: null,
+    username: null
+  }),
+  actions: {
+    login(uid: string, role: string, username?: string) {
+      this.islogin = true
+      this.userid = uid
+      this.username = username || ''
+      this.role = role as Roles
+    },
+    logout() {
+      this.$reset()
+    }
+  }
+})

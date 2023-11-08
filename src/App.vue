1aae64cbf9b5828bb16c233b03ab96472191278c
ZbWeR <1641064392@qq.com>
2023-11-09 00:57:18 +0800

    ✨ feat: svg-icon 全局样式设置

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,14 @@ import { RouterView } from 'vue-router'
 <template>
   <RouterView />
 </template>
+
+<style>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  display: inline-block;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/icons/IconPassword.vue
@@ -0,0 +1,63 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <g opacity="1" transform="translate(0 0)  rotate(0 12 12)">
+      <g opacity="1" transform="translate(4 4)  rotate(0 8 8)">
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(0 0)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(0 6)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(6 0)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(6 6)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(6 12)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(12 0)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+        <path
+          id="圆形 1"
+          fillRule="evenodd"
+          transform="translate(12 6)  rotate(0 2 2)"
+          opacity="1"
+          d="M2,0C0.9,0 0,0.9 0,2C0,3.1 0.9,4 2,4C3.1,4 4,3.1 4,2C4,0.9 3.1,0 2,0Z "
+        />
+      </g>
+    </g>
+  </svg>
+</template>

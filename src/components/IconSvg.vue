97aae7268d9f22892cb0f8e245ec99853f695182
ZbWeR <1641064392@qq.com>
2023-11-13 09:16:15 +0800

    ✨ feat: 增加 iconfont 图标支持(暂时)

---
+++ src/components/IconSvg.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { icon, size, color } = defineProps(['icon', 'size', 'color'])
+</script>
+
+<template>
+  <svg :style="{ width: `${size}px`, height: `${size}px`, color }" class="icon" aria-hidden="true">
+    <use :xlink:href="`#icon-${icon}`"></use>
+  </svg>
+</template>
+
+<style scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  overflow: hidden;
+  display: inline-block;
+  fill: currentColor;
+}
+</style>
